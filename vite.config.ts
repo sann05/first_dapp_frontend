52937e83e0450203a31932161c7f8deb47d5515e
Aliaksandr Sheliutsin <sheliutsin.aliaksandr@gmail.com>
2024-09-13 14:13:39 +0200

    Fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
-  // base: "/first_dapp_frontend/",
+  base: "/first_dapp_frontend/",
 })
